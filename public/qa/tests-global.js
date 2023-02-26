791d8790d82fd9ec866d7d8d19968cfa2d9cec6a
Ruffle <noruffle@gmail.com>
2023-02-26 17:02:42 +0100

    deleted some files

--- public/qa/tests-global.js
+++
@@ -1,7 +0,0 @@
-const { assert } = require("chai")
-
-suite('Global Tests', () => {
-  test(`У данной страницы допустимый заголовок`, () => {
-    assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO');
-  });
-});
--- public/qa/tests-unit.js
+++
@@ -1,8 +0,0 @@
-const fortune = require('../lib/fortune.js');
-const expect = require('chai').expect;
-
-suite('Тесты печений-предсказаний', () => {
-  test('getFortune() должна возвращать предсказание', () => {
-    expect(typeof fortune.getFortune() === 'string');
-  });
-});
